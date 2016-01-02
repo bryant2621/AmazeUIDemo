b0aa0d81d83244eaf4392551ea6bb1aada71f448
bryant2621 <bryant2621@163.com>
2016-01-02 22:44:29 +0800

    Redis
    AddRedisConnection

---
+++ AmazeDemo/Service.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ServiceStack.Redis;
+
+namespace AmazeDemo
+{
+    public partial class Service : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RedisClient client = new RedisClient("192.168.91.128", 6379);
+            client.Set<string>("username", "bryant");
+            var username = client.Get<string>("username");
+            Response.Write(username);
+        }
+    }
+}
